3b4e53d59af57e3cd53f3fd435a79b9356e0f228
Patterbear <benjamin.mcgregor2002@gmail.com>
2025-02-26 10:22:40 +0000

    add get and post event

--- backend/controllers/events.controllers.js
+++ backend/controllers/events.controllers.js
@@ -1,4 +1,8 @@
-const { selectEvents } = require("../models/events.models");
+const {
+  selectEvents,
+  selectEventById,
+  insertEvent,
+} = require("../models/events.models");
 
 exports.getEvents = (req, res, next) => {
   selectEvents()
@@ -6,6 +10,30 @@ exports.getEvents = (req, res, next) => {
       const events = result[0];
 
       res.status(200).send({ events });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getEventById = (req, res, next) => {
+  const { event_id } = req.params;
+
+  selectEventById(event_id)
+    .then((e) => {
+      res.status(200).send({ event: e });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postEvent = (req, res, next) => {
+  const newEvent = req.body;
+
+  insertEvent(newEvent)
+    .then((event) => {
+      res.status(201).send({ event });
     })
     .catch((err) => {
       next(err);
--- backend/routes/events-router.js
+++ backend/routes/events-router.js
@@ -1,9 +1,22 @@
-const { getEvents } = require("../controllers/events.controllers");
+const {
+  getEvents,
+  getEventById,
+  postEvent,
+} = require("../controllers/events.controllers");
 
 const eventsRouter = require("express").Router();
 
-eventsRouter.route("/").get((req, res, next) => {
-  getEvents(req, res, next);
+eventsRouter
+  .route("/")
+  .get((req, res, next) => {
+    getEvents(req, res, next);
+  })
+  .post((req, res, next) => {
+    postEvent(req, res, next);
+  });
+
+eventsRouter.route("/:event_id").get((req, res, next) => {
+  getEventById(req, res, next);
 });
 
 module.exports = eventsRouter;
