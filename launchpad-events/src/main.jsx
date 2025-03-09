cabe28683ce7eb10b34086dfff0c63d60b0c504a
Patterbear <benjamin.mcgregor2002@gmail.com>
2025-03-09 11:17:34 +0000

    add google signin functionality

--- launchpad-events/src/main.jsx
+++ launchpad-events/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId="162294971736-143nfs6g167lcd7p3lj8tlvm8hrjknuk.apps.googleusercontent.com">
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </GoogleOAuthProvider>
+);
--- launchpad-events/src/pages/Home.jsx
+++ launchpad-events/src/pages/Home.jsx
@@ -2,11 +2,44 @@ import { useEffect, useState } from "react";
 import { fetchEvents } from "../../api";
 import EventList from "../components/EventList";
 import SearchBar from "../components/SearchBar";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const Home = () => {
   const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
 
   useEffect(() => {
     fetchEvents()
@@ -41,6 +74,19 @@ const Home = () => {
   return (
     <div className="container mx-auto p-4">
       <SearchBar onSearch={handleSearch} />
+      {profile ? (
+                <div>
+                    <img src={profile.picture} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+            )}
       <EventList events={events} />
     </div>
   );
